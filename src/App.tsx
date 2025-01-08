58d0054b81f8557b28cc217afd683159c5b47fe6
nurhossainfarid <faahsan1516@gmail.com>
2025-01-08 23:26:27 +0600

    setup redux and shadcn

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+    return (
+        <div>
+            <h1>Bootstrapping</h1>
+            <Button>Increase</Button>
+            <p>0</p>
+            <Button>Decrease</Button>
+        </div>
+    )
+};
+
+export default App;
