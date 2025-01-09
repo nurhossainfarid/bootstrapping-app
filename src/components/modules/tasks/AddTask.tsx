38ebe5acab58e69ad319a54cbecf46a8390f66a5
nurhossainfarid <faahsan1516@gmail.com>
2025-01-09 20:13:16 +0600

    create add task

---
+++ src/components/modules/tasks/AddTask.tsx
@@ -0,0 +1,75 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { useForm } from "react-hook-form";
+
+export function AddTask() {
+  const form = useForm();
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>Add Task</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Add Task</DialogTitle>
+          <DialogDescription className="sr-only">
+            Add a new task to your list
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Title</FormLabel>
+                  <FormControl>
+                    <Input {...field} value={field.value || ""} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Textarea {...field} value={field.value || ""} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <DialogFooter>
+              <Button type="submit">Submit</Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- src/pages/tasks.tsx
+++ src/pages/tasks.tsx
@@ -1,3 +1,4 @@
+import { AddTask } from "@/components/modules/tasks/AddTask";
 import TaskCard from "@/components/modules/tasks/TaskCard";
 import { selectTasks } from "@/redux/features/tasks/taskSlice";
 import { useSelector } from "react-redux";
@@ -7,8 +8,9 @@ const Tasks = () => {
 
   return (
     <div className="mx-auto max-w-7xl px-5 mt-20">
-      <div>
+      <div className="flex justify-between items-center">
         <h1>Tasks</h1>
+        <AddTask />
       </div>
       <div className="flex flex-col gap-5 mt-5">
         {tasks.map((task) => (
